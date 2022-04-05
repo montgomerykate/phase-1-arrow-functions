5fbf0f526ccd3ab9b9cae89ae197257dcf1a6866
Kate Montgomery <katemontgomery431@gmail.com>
2022-04-05 11:36:12 -0400

    commit tree b67f6a7e5d1853eb3778566b93adc8e94181d6f5

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide(parameter1, parameter2) {
+    return (2000/100)
+}
+const square = (parameter1, parameter2) => {
+    console.log(parameter1=2)
+    console.log(parameter2=2)
+    return parameter1*parameter2
+}
+const add = (parameter1,parameter2) => {
+    console.log(parameter1)
+    console.log(parameter2)
+    return parameter1+parameter2
+}
